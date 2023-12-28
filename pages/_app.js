3b28aa793a588856d19331a50c8038d7c13fd72d
Kumaraswamy B G <care.xoma@gmail.com>
2023-12-28 12:52:43 +0530

    Add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta property="og:image" content="https://themelon.space/nature.jpg"/>
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
